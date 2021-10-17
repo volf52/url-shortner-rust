e9429ef88cfca9123fe67a13fc4fc0d16fd3e80f
Arslan <rslnkrmt2552@gmail.com>
2021-10-17 03:45:28 +0300

    Add basic tests

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/api/<key>")]
+#[get("/api/get?<key>")]
 fn redirect(key: u32, state: &RocketState) -> Result<Redirect, NotFound<&str>> {
     state.get(&key).map(|url| Redirect::to(url.clone())).ok_or(NotFound("Invalid or expired link!"))
 }
@@ -36,4 +36,36 @@ fn shorten(url: String, state: &RocketState) -> Result<String, BadRequest<&str>>
 fn rocket() -> _ {
     rocket::build().manage(DashMap::<u32, String>::new())
         .mount("/", routes![index, redirect, shorten])
+}
+
+#[cfg(test)]
+mod tests {
+    use rocket::{http::Status, local::blocking::Client};
+
+    use super::rocket;
+
+    #[test]
+    fn simple_demo_test() {
+        let x = 1 + 1;
+        assert_eq!(x, 2)
+    }
+
+    #[test]
+    fn valid_requests() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+
+        let response = client.post("/api/shorten?url=https://duck.com").dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+
+        let key: u32 = response.into_string().expect("body").parse().expect("valid u32");
+
+        let response = client.get(format!("/api/get?key={}", key)).dispatch();
+
+        assert_eq!(response.status(), Status::SeeOther);
+
+        let redirect = response.headers().get_one("Location").expect("location header");
+
+        assert_eq!(redirect, "https://duck.com")
+    }
 }
